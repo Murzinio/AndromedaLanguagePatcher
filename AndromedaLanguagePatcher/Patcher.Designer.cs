b8f0a29287ff0b3d9ced3a3c1b139917424ff8ee
Murzinio <k.pawelczykbsh@gmail.com>
2017-03-25 13:57:07 +0100

    Add project files.

---
+++ AndromedaLanguagePatcher/Patcher.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace AndromedaLanguagePatcher
+{
+	partial class Patcher
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Patcher));
+			this.comboBox_GameLanguage = new System.Windows.Forms.ComboBox();
+			this.label_GameLanguage = new System.Windows.Forms.Label();
+			this.button_Patch = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBox_Path = new System.Windows.Forms.TextBox();
+			this.button_Path = new System.Windows.Forms.Button();
+			this.button_Restore = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// comboBox_GameLanguage
+			// 
+			this.comboBox_GameLanguage.FormattingEnabled = true;
+			this.comboBox_GameLanguage.Items.AddRange(new object[] {
+            "Polish",
+            "Russian"});
+			this.comboBox_GameLanguage.Location = new System.Drawing.Point(44, 25);
+			this.comboBox_GameLanguage.Name = "comboBox_GameLanguage";
+			this.comboBox_GameLanguage.Size = new System.Drawing.Size(121, 21);
+			this.comboBox_GameLanguage.TabIndex = 0;
+			this.comboBox_GameLanguage.SelectedIndexChanged += new System.EventHandler(this.comboBox_GameLanguage_SelectedIndexChanged);
+			// 
+			// label_GameLanguage
+			// 
+			this.label_GameLanguage.AutoSize = true;
+			this.label_GameLanguage.Location = new System.Drawing.Point(41, 9);
+			this.label_GameLanguage.Name = "label_GameLanguage";
+			this.label_GameLanguage.Size = new System.Drawing.Size(105, 13);
+			this.label_GameLanguage.TabIndex = 1;
+			this.label_GameLanguage.Text = "Your game language";
+			// 
+			// button_Patch
+			// 
+			this.button_Patch.Enabled = false;
+			this.button_Patch.Location = new System.Drawing.Point(377, 106);
+			this.button_Patch.Name = "button_Patch";
+			this.button_Patch.Size = new System.Drawing.Size(75, 23);
+			this.button_Patch.TabIndex = 2;
+			this.button_Patch.Text = "Patch";
+			this.button_Patch.UseVisualStyleBackColor = true;
+			this.button_Patch.Click += new System.EventHandler(this.button_Patch_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(41, 49);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(58, 13);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "ME:A Path";
+			// 
+			// textBox_Path
+			// 
+			this.textBox_Path.Enabled = false;
+			this.textBox_Path.Location = new System.Drawing.Point(44, 65);
+			this.textBox_Path.Name = "textBox_Path";
+			this.textBox_Path.Size = new System.Drawing.Size(300, 20);
+			this.textBox_Path.TabIndex = 4;
+			// 
+			// button_Path
+			// 
+			this.button_Path.Enabled = false;
+			this.button_Path.Location = new System.Drawing.Point(377, 63);
+			this.button_Path.Name = "button_Path";
+			this.button_Path.Size = new System.Drawing.Size(75, 23);
+			this.button_Path.TabIndex = 5;
+			this.button_Path.Text = "Browse";
+			this.button_Path.UseVisualStyleBackColor = true;
+			this.button_Path.Click += new System.EventHandler(this.button_Path_Click);
+			// 
+			// button_Restore
+			// 
+			this.button_Restore.Enabled = false;
+			this.button_Restore.Location = new System.Drawing.Point(44, 106);
+			this.button_Restore.Name = "button_Restore";
+			this.button_Restore.Size = new System.Drawing.Size(121, 23);
+			this.button_Restore.TabIndex = 6;
+			this.button_Restore.Text = "Restore original exe";
+			this.button_Restore.UseVisualStyleBackColor = true;
+			this.button_Restore.Click += new System.EventHandler(this.button_Restore_Click);
+			// 
+			// Patcher
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(464, 141);
+			this.Controls.Add(this.button_Restore);
+			this.Controls.Add(this.button_Path);
+			this.Controls.Add(this.textBox_Path);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button_Patch);
+			this.Controls.Add(this.label_GameLanguage);
+			this.Controls.Add(this.comboBox_GameLanguage);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MaximumSize = new System.Drawing.Size(480, 180);
+			this.MinimumSize = new System.Drawing.Size(480, 180);
+			this.Name = "Patcher";
+			this.Text = "Andromeda Language Patcher";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ComboBox comboBox_GameLanguage;
+		private System.Windows.Forms.Label label_GameLanguage;
+		private System.Windows.Forms.Button button_Patch;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox textBox_Path;
+		private System.Windows.Forms.Button button_Path;
+		private System.Windows.Forms.Button button_Restore;
+	}
+}
+
---
+++ AndromedaLanguagePatcher/Patcher.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+
+namespace AndromedaLanguagePatcher
+{
+	enum GameLanguage
+	{
+		Polish,
+		Russian
+	}
+	public partial class Patcher : Form
+	{
+		string exePath;
+		
+		byte[] exePatched;
+		GameLanguage gameLanguage;
+
+		//offsets to change, first in array is all small letters - like "pl_pl"
+		int[] bytesToOriginalLanguageOffset = new int[2];
+		int[] bytesToEnglishOffset = new int[2];
+
+		public Patcher()
+		{
+			InitializeComponent();
+		}
+
+		private bool DoesBackupExeExist()
+		{
+			return File.Exists(exePath + ".backup");
+		}
+
+		private void BackupExe()
+		{
+			if (DoesBackupExeExist())
+				File.Delete(exePath + ".backup");
+			File.Copy(exePath, exePath + ".backup", true);
+		}
+
+		private void ReadExe()
+		{
+			byte[] exeRead;
+			using (BinaryReader reader = new BinaryReader(File.Open(exePath, FileMode.Open)))
+			{
+				//exe = reader.ReadBytes(int.MaxValue);
+				const int bufferSize = 4096;
+				using (var ms = new MemoryStream())
+				{
+					byte[] buffer = new byte[bufferSize];
+					int count;
+					while ((count = reader.Read(buffer, 0, buffer.Length)) != 0)
+						ms.Write(buffer, 0, count);
+					exeRead =  ms.ToArray();
+				}
+			}
+			
+
+
+
+			switch (gameLanguage)
+			{
+				case GameLanguage.Polish:
+					for (int i = 0; i < exeRead.Length - 3; i++)
+					{
+						if (exeRead[i] == 0x70 && exeRead[i + 1] == 0x6C && exeRead[i + 2] == 0x5F && exeRead[i + 3] == 0x70 && exeRead[i + 4] == 0x6C)
+							bytesToEnglishOffset[0] = i;
+						else if (exeRead[i] == 0x70 && exeRead[i + 1] == 0x6C && exeRead[i + 2] == 0x5F && exeRead[i + 3] == 0x50 && exeRead[i + 4] == 0x4C)
+							bytesToEnglishOffset[1] = i;
+						else if (exeRead[i] == 0x65 && exeRead[i + 1] == 0x6E && exeRead[i + 2] == 0x5F && exeRead[i + 3] == 0x75 && exeRead[i + 4] == 0x73)
+							bytesToOriginalLanguageOffset[0] = i;
+						else if (exeRead[i] == 0x65 && exeRead[i + 1] == 0x6E && exeRead[i + 2] == 0x5F && exeRead[i + 3] == 0x55 && exeRead[i + 4] == 0x53)
+							bytesToOriginalLanguageOffset[1] = i;
+					}
+					break;
+				case GameLanguage.Russian:
+					for (int i = 0; i < exeRead.Length; i++)
+					{
+						if (exeRead[i] == 0x72 && exeRead[i + 1] == 0x75 && exeRead[i + 2] == 0x5F && exeRead[i + 3] == 0x72 && exeRead[i + 4] == 0x75)
+							bytesToEnglishOffset[0] = i;
+						else if (exeRead[i] == 0x72 && exeRead[i + 1] == 0x75 && exeRead[i + 2] == 0x5F && exeRead[i + 3] == 0x52 && exeRead[i + 4] == 0x55)
+							bytesToEnglishOffset[1] = i;
+						else if (exeRead[i] == 0x65 && exeRead[i + 1] == 0x6E && exeRead[i + 2] == 0x5F && exeRead[i + 3] == 0x75 && exeRead[i + 4] == 0x73)
+							bytesToOriginalLanguageOffset[0] = i;
+						else if (exeRead[i] == 0x65 && exeRead[i + 1] == 0x6E && exeRead[i + 2] == 0x5F && exeRead[i + 3] == 0x55 && exeRead[i + 4] == 0x53)
+							bytesToOriginalLanguageOffset[1] = i;
+					}
+					break;
+			}
+			exePatched = exeRead;
+		}
+		private void PatchExe()
+		{
+			//actual bytes to write, first in array is all small letters - like "pl_pl"
+			byte[] bytesOriginalLanguageLower = new byte[5];
+			byte[] bytesOriginalLanguageUpper = new byte[5];
+			byte[] bytesEnglishLower = new byte[5] { 0x65, 0x6E, 0x5F, 0x75, 0x73 };
+			byte[] bytesEnglishUpper = new byte[5] { 0x65, 0x6E, 0x5F, 0x55, 0x53 };
+
+			switch (gameLanguage)
+			{
+				case GameLanguage.Polish:
+					bytesOriginalLanguageLower[0] = 0x70;
+					bytesOriginalLanguageLower[1] = 0x6C;
+					bytesOriginalLanguageLower[2] = 0x5F;
+					bytesOriginalLanguageLower[3] = 0x70;
+					bytesOriginalLanguageLower[4] = 0x6C;
+
+					bytesOriginalLanguageUpper[0] = 0x70;
+					bytesOriginalLanguageUpper[1] = 0x6C;
+					bytesOriginalLanguageUpper[2] = 0x5F;
+					bytesOriginalLanguageUpper[3] = 0x50;
+					bytesOriginalLanguageUpper[4] = 0x4C;
+					break;
+				case GameLanguage.Russian:
+					bytesOriginalLanguageLower[0] = 0x72;
+					bytesOriginalLanguageLower[1] = 0x75;
+					bytesOriginalLanguageLower[2] = 0x5F;
+					bytesOriginalLanguageLower[3] = 0x72;
+					bytesOriginalLanguageLower[4] = 0x75;
+
+					bytesOriginalLanguageUpper[0] = 0x72;
+					bytesOriginalLanguageUpper[1] = 0x75;
+					bytesOriginalLanguageUpper[2] = 0x5F;
+					bytesOriginalLanguageUpper[3] = 0x52;
+					bytesOriginalLanguageUpper[4] = 0x55;
+					break;
+			}
+			
+			using (BinaryWriter writer = new BinaryWriter(File.Open(exePath, FileMode.Open)))
+			{
+
+				for (int i = 0; i < 5; i++)
+				{
+					writer.BaseStream.Position = bytesToEnglishOffset[0] + i;
+					writer.Write(bytesEnglishLower[i]);
+
+					writer.BaseStream.Position = bytesToOriginalLanguageOffset[0] + i;
+					writer.Write(bytesOriginalLanguageLower[i]);
+
+					writer.BaseStream.Position = bytesToEnglishOffset[1] + i;
+					writer.Write(bytesEnglishUpper[i]);
+
+					writer.BaseStream.Position = bytesToOriginalLanguageOffset[1] + i;
+					writer.Write(bytesOriginalLanguageUpper[i]);
+				}
+			}
+		}
+
+		private void comboBox_GameLanguage_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			button_Path.Enabled = true;
+			gameLanguage = (GameLanguage)comboBox_GameLanguage.SelectedIndex;
+		}
+
+		private void button_Path_Click(object sender, EventArgs e)
+		{
+			using (var folderBrowser = new FolderBrowserDialog())
+			{
+				bool pathSelected = false;
+				bool aborted = false;
+				
+				while (!pathSelected && !aborted)
+				{
+					DialogResult resultBrowse = folderBrowser.ShowDialog();
+					
+					if (resultBrowse == DialogResult.OK && !string.IsNullOrWhiteSpace(folderBrowser.SelectedPath))
+					{
+						exePath = folderBrowser.SelectedPath + "\\MassEffectAndromeda.exe";
+						if (!File.Exists(exePath))
+						{
+							DialogResult resultMessage = MessageBox.Show("Wrong path, MassEffectAndromeda.exe not found.", "Andromeda Language Patcher",
+							MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
+
+							if (resultMessage == DialogResult.Cancel)
+								aborted = true;
+						}
+						else
+						{
+							pathSelected = true;
+							textBox_Path.Text = exePath;
+							button_Patch.Enabled = true;
+						}
+							
+					}
+					else
+						aborted = true;
+				}	
+			}
+			if (DoesBackupExeExist())
+				button_Restore.Enabled = true;
+		}
+
+		private void button_Patch_Click(object sender, EventArgs e)
+		{
+			BackupExe();
+			button_Restore.Enabled = true;
+
+			ReadExe();
+			PatchExe();
+
+			DialogResult resultMessage = MessageBox.Show("Language changed.", "Andromeda Language Patcher",
+			MessageBoxButtons.OK, MessageBoxIcon.Information);
+		}
+
+		private void button_Restore_Click(object sender, EventArgs e)
+		{
+			File.Delete(exePath);
+			File.Move(exePath + ".backup", exePath);
+
+			button_Restore.Enabled = false;
+			DialogResult resultMessage = MessageBox.Show("Original exe restored.", "Andromeda Language Patcher",
+			MessageBoxButtons.OK, MessageBoxIcon.Information);
+		}
+	}
+}
